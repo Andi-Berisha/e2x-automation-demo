773155758e7913193f1faf10863084bde51b60dc
Andi Berisha <andiberisha@hollandandbarrett.com>
2023-03-19 15:08:21 +0000

    fix: removed storage state

--- src/fixtures/test.ts
+++ src/fixtures/test.ts
@@ -1,6 +1,5 @@
 import { test as base } from "@playwright/test";
 import { HomePage } from "../pages/homePage";
-import { StorageState } from "../fixtures/storageState";
 import { ProductListingPage } from "../pages/productListingPage";
 import { CartPage } from "../pages/cartPage";
 import { ProductDisplayPage } from "../pages/productDisplayPage";
@@ -14,17 +13,9 @@ type CustomFixtures = {
   PDP: ProductDisplayPage;
   checkout: CheckoutPage;
   orderConfirm: OrderConirmPage;
-  // storageState: StorageState;
 };
 
 export const test = base.extend<CustomFixtures>({
-  // page: async ({ browser, baseURL }, use) => {
-  //   const state = new StorageState(baseURL);
-  //   const context = await browser.newContext({ storageState: state.getState });
-  //   const page = await context.newPage();
-  //   await use(page);
-  // },
-
   home: async ({ page }, use) => {
     await use(new HomePage(page));
   },
